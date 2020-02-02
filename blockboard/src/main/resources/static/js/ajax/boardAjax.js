14621aa950c50795e620b12ac17deb19d8d0ff79
Dong-wook94 <gygacpu@naver.com>
2020-02-02 18:32:08 +0900

    ajax 파일에서 UI 업데이트 파일분리

--- blockboard/src/main/resources/static/js/ajax/boardAjax.js
+++ blockboard/src/main/resources/static/js/ajax/boardAjax.js
@@ -3,51 +3,7 @@
  * @file boardAjax.js
  */
 
-//새로운 탭 내용으로 교체
-function updateTab(data) {
-    var source = $('#boards-template').html();
-    var template = Handlebars.compile(source);
-    var board = {boards: data};
-    var itemList = template(board);
-    $('#tab_id').html(itemList);
-}
 
-//게시글 내용
-function loadPostContent(data) {
-    var source = $('#postcontent-template').html();
-    var template = Handlebars.compile(source);
-    var post = {post: data};
-    var item = template(post);
-    $('#postcontent').html(item);
-}
-
-//게시글 목록
-function loadPostList(data) {
-    var source = $('#posts-template').html();
-    var template = Handlebars.compile(source);
-    var post = {posts: data};
-    var itemList = template(post);
-    $('#postlist').html(itemList);
-
-}
-
-//삭제를 위한 UI
-function getBoardListToDelete(data) {
-    var source = $('#deleteboards-template').html();
-    var template = Handlebars.compile(source);
-    var boardList = {boards: data};
-    var itemList = template(boardList);
-    $('.modal-body-deleteBoard').html(itemList);
-}
-
-//이름변경을 위한 UI
-function getBoardListToChangeName(data) {
-    var source = $('#changeBoardName-template').html();
-    var template = Handlebars.compile(source);
-    var boardList = {boards: data};
-    var itemList = template(boardList);
-    $('.modal-body-changeBoardName').html(itemList);
-}
 
 
 //탭 업데이트 새로운 게시판 목록으로
