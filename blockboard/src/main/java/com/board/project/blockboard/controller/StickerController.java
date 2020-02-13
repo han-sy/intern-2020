4d9e8be5639d400f6252d96831544245bfd1619c
myabcc17 <myabcc17@naver.com>
2020-02-13 15:59:37 +0900

    Sticker 캐쉬 적용 구현

---
+++ blockboard/src/main/java/com/board/project/blockboard/controller/StickerController.java
@@ -0,0 +1,35 @@
+package com.board.project.blockboard.controller;
+
+import com.board.project.blockboard.service.StickerService;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Woohyeok Jun <woohyeok.jun@worksmobile.com>
+ * @file StickerController.java
+ */
+@Slf4j
+@RestController
+public class StickerController {
+
+  @Autowired
+  private StickerService stickerService;
+
+  @GetMapping("/sticker")
+  public JSONObject getStickers(HttpServletRequest request) {
+    return stickerService.getStickers(request);
+  }
+
+  @GetMapping(value = "/sticker/{directory}/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
+  public byte[] getSticker(@PathVariable("directory") String dirName,
+      @PathVariable("fileName") String fileName) throws IOException {
+    return stickerService.getSticker(dirName, fileName);
+  }
+}
---
+++ blockboard/src/main/resources/static/ckeditor/config.js
@@ -0,0 +1,83 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function (config) {
+  // Define changes to default configuration here.
+  // For complete reference see:
+  // https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+
+  // The toolbar groups arrangement, optimized for a single toolbar row.
+  config.toolbarGroups = [
+    {name: 'document', groups: ['mode', 'document', 'doctools']},
+    {name: 'clipboard', groups: ['clipboard', 'undo']},
+    {name: 'editing', groups: ['find', 'selection', 'spellchecker']},
+    {name: 'forms'},
+    {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+    {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi']},
+    {name: 'links'},
+    {name: 'insert'},
+    {name: 'styles'},
+    {name: 'colors'},
+    {name: 'tools'},
+    {name: 'others'},
+    {name: 'about'}
+  ];
+
+  // 에디터의 높이
+  config.height = 400;
+
+  // 에디터에서 ENTER 키 처리를 <br>로 하게 한다.
+  config.enterMode = CKEDITOR.ENTER_BR;
+
+  // 에디터에서 '파일찾기' 할 때의 경로
+  config.filebrowserImageUploadUrl = "/imageUpload";
+
+  // 에디터의 추가 플러그인 설정
+  config.extraPlugins = 'dialog, '
+      + 'filebrowser, '
+      + 'filetools, '
+      + 'popup, '
+      + 'widget, '
+      + 'widgetselection, '
+      + 'lineutils, '
+      + 'contextmenu, '
+      + 'menu, '
+      + 'floatpanel, '
+      + 'panel, '
+      + 'uploadimage, '
+      + 'uploadwidget,'
+      + 'notificationaggregator,'
+      + 'floatpanel,'
+      + 'panelbutton,'
+      + 'button,'
+      + 'xml,'
+      + 'ajax';
+
+  if ($('#functionAble4').attr("value") == "on") {
+    config.extraPlugins = "image2";
+  }
+
+  if ($('#functionAble6').attr("value") == "on") {
+    config.extraPlugins = "emoji";
+  }
+
+  // The default plugins included in the basic setup define some buttons that
+  // are not needed in a basic editor. They are removed here.
+	config.removeButtons = 'Strike,Subscript,Superscript';
+
+	CKEDITOR.on('dialogDefinition', function (evt) {
+		var dialog = evt.data;
+
+		if (dialog.name == 'sticker') {
+			// Get dialog definition.
+			var dialogDefinition = evt.data.definition;
+
+			console.log(dialogDefinition);
+			dialogDefinition.dialog.resize(300,200);
+		}
+	});
+  // Dialog windows are also simplified.
+  config.removeDialogTabs = 'link:advanced';
+};
